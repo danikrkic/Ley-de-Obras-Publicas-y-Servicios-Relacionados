20d1ed581243fe264b46a39c467179811086875d
Daniel López Domínguez <38300588+danikrkic@users.noreply.github.com>
2026-06-08 13:06:00 -0600

    mas cambios

---
+++ Proyecto FEPI/frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <div className="card shadow p-4">
+        <h1 className="text-primary">
+          Proyecto FEPI 🚀
+        </h1>
+
+        <p>
+          React + Vite + Bootstrap + Django + PostgreSQL 18 funcionando.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
